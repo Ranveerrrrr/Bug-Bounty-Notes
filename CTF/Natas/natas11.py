202bfd529994dc7e47191d6a2f930ae3017dfed1
Ranveer <ranveer121210@gmail.com>
2026-02-24 12:29:22 +0530

    vault backup: 2026-02-24 12:29:22

--- CTF/Natas/natas11.py
+++ CTF/Natas/natas11.py
@@ -11,3 +11,13 @@ print(data_cookie)
 cookie= 'HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJAyIxTRg='
 b_d= base64.b64decode(cookie).decode()
 print(b_d)
+
+text = '{"showpassword"=>"no", "bgcolor"=>"#ffffff"}'
+
+def xor_bytes(data: bytes, key: bytes) -> bytes:
+    return bytes(
+        b ^ key[i % len(key )]
+        for i,b in enumeration(data)
+    )
+
+xor_bytes(text, b_d)
