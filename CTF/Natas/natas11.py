3fc680818d08f0581ed327b0ce5600a7e1709810
Ranveer <ranveer121210@gmail.com>
2026-02-24 12:17:43 +0530

    vault backup: 2026-02-24 12:17:43

--- CTF/Natas/natas11.py
+++ CTF/Natas/natas11.py
@@ -1,8 +1,9 @@
 import requests
+import base64
 
 url = "http://natas11.natas.labs.overthewire.org/"
 
 response = requests.get(url, auth=("natas11", "UJdqkK1pTu6VLt9UHWAgRZz6sVUZ3lEk"))
 
-print(response.cookies['data'])
+print(base64.b64decode(response.cookies['data']))
 
