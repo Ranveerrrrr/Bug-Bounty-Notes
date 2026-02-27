5fa468bdfddfa1f93e1f3f41cfbc675421ff365e
Ranveer <ranveer121210@gmail.com>
2026-02-27 15:53:21 +0530

    vault backup: 2026-02-27 15:53:21

--- CTF/Natas/natas11.py
+++ CTF/Natas/natas11.py
@@ -20,9 +20,3 @@ def xor_bytes(a: bytes, b: bytes) -> bytes:
 raw = xor_bytes(b_d, text)
 print("Key: ",raw)
 
-text_d = b'eyJzaG93cGFzc3dvcmQiPT4ieWVzIiwgImJnY29sb3IiPT4iI2ZmZmZmZiJ9'
-key = b'eDWo'
-
-encoded = bytes(b ^ key[i % len(key)] for i, b in enumerate(text_d))
-
-print(encoded.hex())
