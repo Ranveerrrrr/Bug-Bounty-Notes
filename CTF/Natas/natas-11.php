8788a719c8000e3602331881c9a302e485020345
Ranveer <ranveer121210@gmail.com>
2026-02-28 14:02:43 +0530

    vault backup: 2026-02-28 14:02:43

--- CTF/Natas/natas-11.php
+++ CTF/Natas/natas-11.php
@@ -1,3 +1,4 @@
+to find out the key
 <?
 
 function xor_encrypt($in) {
@@ -15,5 +16,26 @@ function xor_encrypt($in) {
 
 $cookie = "HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJAyIxTRg%3D";
 echo xor_encrypt(base64_decode($cookie))
+
+?>
+
+
+to create new cookie
+<?
+
+function xor_encrypt($in) {
+    $key = "eDWo";
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+echo base64_encode(xor_encrypt(json_encode(array("showpassword"=>"yes", "bgcolor"=>"#ffffff"))))
 
 ?>
